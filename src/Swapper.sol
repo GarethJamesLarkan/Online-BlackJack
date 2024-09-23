b6eb20739f76dc1c10550f382925f3d860e439a6
Myles Traut <myles.traut1@gmail.com>
2024-09-23 13:17:20 +0200

    working

--- src/Swapper.sol
+++ src/Swapper.sol
@@ -2,10 +2,10 @@
 pragma solidity 0.8.22;
 
 import {IUniversalRouter} from "lib/universal-router/contracts/interfaces/IUniversalRouter.sol";
-import {IUniswapV3Factory} from "lib/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import {IUniswapV2Factory} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import {Commands} from "lib/universal-router/contracts/libraries/Commands.sol";
 import {Constants} from "lib/universal-router/contracts/libraries/Constants.sol";
-import {Permit2} from 'lib/permit2/src/Permit2.sol';
+import 'lib/permit2/src/interfaces/IPermit2.sol';
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Swapper {
@@ -14,13 +14,13 @@ contract Swapper {
     error AmountIsZero();
 
     IUniversalRouter public immutable universalRouter;
-    IUniswapV3Factory public immutable uniswapV3Factory;
-    Permit2 public immutable permit2;
+    IUniswapV2Factory public immutable uniswapV2Factory;
+    IPermit2 public permit2;
 
-    constructor(address _universalRouter, address _uniswapV3Factory, address _permit2) {
+    constructor(address _universalRouter, address _uniswapV2Factory, address _permit2) {
         universalRouter = IUniversalRouter(_universalRouter);
-        uniswapV3Factory = IUniswapV3Factory(_uniswapV3Factory);
-        permit2 = Permit2(_permit2);
+        uniswapV2Factory = IUniswapV2Factory(_uniswapV2Factory);
+        permit2 = IPermit2(_permit2);
     }
 
     function swap(address _tokenIn, address _tokenOut, uint256 _amountIn) external checkAddress(_tokenIn) checkAddress(_tokenOut) {
@@ -30,20 +30,25 @@ contract Swapper {
 
         ERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
 
-        address pool = uniswapV3Factory.getPool(_tokenIn, _tokenOut, 3000);
-        if (pool == address(0)) {
+        // address pool = uniswapV3Factory.getPool(_tokenIn, _tokenOut, 3000);
+        // if (pool == address(0)) {
+        //     revert PoolDoesNotExist();
+        // }
+
+        address pair = uniswapV2Factory.getPair(_tokenIn, _tokenOut);
+        if (pair == address(0)) {
             revert PoolDoesNotExist();
         }
 
+        ERC20(_tokenIn).approve(address(permit2), type(uint160).max);
         permit2.approve(_tokenIn, address(universalRouter), type(uint160).max, type(uint48).max);
-        permit2.approve(_tokenOut, address(universalRouter), type(uint160).max, type(uint48).max);
 
-        bytes memory commands = abi.encodePacked(bytes1(uint8(Commands.V3_SWAP_EXACT_IN)));
+        bytes memory commands = abi.encodePacked(bytes1(uint8(Commands.V2_SWAP_EXACT_IN)));
         address[] memory path = new address[](2);
         path[0] = _tokenIn;
         path[1] = _tokenOut;
         bytes[] memory inputs = new bytes[](1);
-        inputs[0] = abi.encode(msg.sender, _amountIn, _amountIn, path, false);
+        inputs[0] = abi.encode(Constants.MSG_SENDER, _amountIn, 0, path, true);
 
         universalRouter.execute(commands, inputs, block.timestamp + 1000);
     }
--- test/TestSetup.t.sol
+++ test/TestSetup.t.sol
@@ -1,69 +1,159 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-import {Test, console} from "forge-std/Test.sol";
+import 'forge-std/Test.sol';
+import 'permit2/src/interfaces/IPermit2.sol';
+import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import {IUniswapV2Factory} from '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
+import {IUniswapV2Pair} from '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import {IUniversalRouter} from "lib/universal-router/contracts/interfaces/IUniversalRouter.sol";
+import {Constants} from 'lib/universal-router/contracts/libraries/Constants.sol';
+import {Commands} from 'lib/universal-router/contracts/libraries/Commands.sol';
+
 import {Swapper} from "../src/Swapper.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Permit2} from 'lib/permit2/src/Permit2.sol';
 
-import {IUniswapV3Factory} from "lib/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import '@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol';
 
-contract TestSetup is Test {
-    uint256 mainnetFork;
+contract UniswapV2Test is Test {
+    uint256 constant AMOUNT = 1e6;
+    uint256 constant BALANCE = 100000 ether;
+    IUniswapV2Factory constant FACTORY = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+    ERC20 constant WETH9 = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    ERC20 constant USDC = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IPermit2 constant PERMIT2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
 
-    IUniswapV3Factory public uniswapV3Factory;
+    IUniversalRouter router = IUniversalRouter(0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD);
 
     Swapper public swapper;
-    Permit2 public PERMIT2;
 
-    address public universalRouter = 0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD;
-    address public uniswapV3FactoryAddress = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
-    address public permit2Address = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+    address alice = vm.addr(1001);
 
-    address public weth9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address public usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address public uni = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
+    function setUp() public virtual {
+        vm.createSelectFork(vm.envString('MAINNET_RPC_URL'));
 
-    address public alice = makeAddr("alice");
-    address public bob = makeAddr("bob");
-
-    function setUp() public {
-        vm.selectFork(vm.createFork(vm.envString("MAINNET_RPC_URL")));
-
-        deal(alice, 10 ether);
-
-        PERMIT2 = Permit2(permit2Address);
-
-        swapper = new Swapper(universalRouter, uniswapV3FactoryAddress, permit2Address);
-        uniswapV3Factory = IUniswapV3Factory(uniswapV3FactoryAddress);
+        swapper = new Swapper(address(router), address(FACTORY), address(PERMIT2));
 
         vm.startPrank(alice);
-        ERC20(weth9).approve(address(swapper), type(uint256).max);
-        ERC20(usdc).approve(address(swapper), type(uint256).max);
-        ERC20(weth9).approve(address(PERMIT2), type(uint256).max);
-        ERC20(usdc).approve(address(PERMIT2), type(uint256).max);
-        PERMIT2.approve(weth9, address(universalRouter), type(uint160).max, type(uint48).max);
-        PERMIT2.approve(usdc, address(universalRouter), type(uint160).max, type(uint48).max);
+        deal(alice, BALANCE);
+        deal(address(USDC), alice, BALANCE);
+        deal(address(WETH9), alice, BALANCE);
+
+        ERC20(USDC).approve(address(swapper), type(uint256).max);
+        ERC20(WETH9).approve(address(swapper), type(uint256).max);
+        ERC20(USDC).approve(address(PERMIT2), type(uint256).max);
+        ERC20(WETH9).approve(address(PERMIT2), type(uint256).max);
+        PERMIT2.approve(address(USDC), address(router), type(uint160).max, type(uint48).max);
+        PERMIT2.approve(address(WETH9), address(router), type(uint160).max, type(uint48).max);
+
+        ERC20(USDC).approve(address(PERMIT2), type(uint160).max);
+        vm.stopPrank();
     }
 
-    function test_Deploy() public {
-        assertEq(address(swapper.universalRouter()), universalRouter);
-        assertEq(address(swapper.uniswapV3Factory()), address(uniswapV3Factory));
-    }
+    function test_ExactInput0For1() public {
 
-    function test_Swap() public {
-        
-        assertEq(vm.activeFork(), mainnetFork);
-        
-        console.log(uniswapV3Factory.getPool(weth9, usdc, 3000));
-        deal(weth9, alice, 10 ether);
-        assertEq(ERC20(weth9).balanceOf(alice), 10 ether);
-        assertEq(ERC20(usdc).balanceOf(alice), 0);
+        console.log(ERC20(address(USDC)).balanceOf(alice));
+        console.log(ERC20(address(WETH9)).balanceOf(address(swapper)));
+        bytes memory commands = abi.encodePacked(bytes1(uint8(Commands.V2_SWAP_EXACT_IN)));
+        address[] memory path = new address[](2);
+        path[0] = address(USDC);
+        path[1] = address(WETH9);
+        bytes[] memory inputs = new bytes[](1);
+        inputs[0] = abi.encode(Constants.MSG_SENDER, AMOUNT, 0, path, true);
 
-        vm.startPrank(alice);
-        swapper.swap(weth9, usdc, 1 ether);
+        vm.prank(alice);
+        swapper.swap(address(USDC), address(WETH9), AMOUNT);
+
+        console.log(ERC20(address(USDC)).balanceOf(alice));
+        console.log(ERC20(address(WETH9)).balanceOf(address(swapper)));
+        //router.execute(commands, inputs, block.timestamp + 10000);
+        //assertEq(ERC20(address(USDC)).balanceOf(alice), BALANCE - AMOUNT);
+        // assertGt(ERC20(address(WETH9)).balanceOf(alice), BALANCE);
+
+    //     ISwapRouter.ExactInputSingleParams memory params =
+    //         ISwapRouter.ExactInputSingleParams({
+    //             tokenIn: DAI,
+    //             tokenOut: WETH9,
+    //             fee: poolFee,
+    //             recipient: msg.sender,
+    //             deadline: block.timestamp,
+    //             amountIn: amountIn,
+    //             amountOutMinimum: 0,
+    //             sqrtPriceLimitX96: 0
+    //         });
     }
 }
+
+// import {Test, console} from "forge-std/Test.sol";
+// import {Swapper} from "../src/Swapper.sol";
+// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import {IPermit2} from 'lib/permit2/src/interfaces/IPermit2.sol';
+
+// import {IUniswapV3Factory} from "lib/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+// import {IUniswapV2Factory} from "lib/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+
+// contract TestSetup is Test {
+//     uint256 mainnetFork;
+
+//     IUniswapV3Factory public uniswapV3Factory;
+//     IUniswapV2Factory public uniswapV2Factory;
+//     Swapper public swapper;
+//     IPermit2 public PERMIT2;
+
+//     address public universalRouter = 0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD;
+//     address public uniswapV3FactoryAddress = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+//     address public uniswapV2FactoryAddress = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+//     address public permit2Address = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+
+//     address public weth9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+//     address public usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+//     address public uni = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
+
+//     address public alice = makeAddr("alice");
+//     address public bob = makeAddr("bob");
+
+//     function setUp() public {
+//         vm.selectFork(vm.createFork(vm.envString("MAINNET_RPC_URL")));
+
+//         deal(alice, 10 ether);
+
+//         PERMIT2 = IPermit2(permit2Address);
+
+//         swapper = new Swapper(universalRouter, uniswapV2FactoryAddress, uniswapV3FactoryAddress, permit2Address);
+//         uniswapV3Factory = IUniswapV3Factory(uniswapV3FactoryAddress);
+//         uniswapV2Factory = IUniswapV2Factory(uniswapV2FactoryAddress);
+
+//         vm.startPrank(alice);
+//         ERC20(weth9).approve(address(swapper), type(uint256).max);
+//         ERC20(usdc).approve(address(swapper), type(uint256).max);
+//         ERC20(weth9).approve(address(PERMIT2), type(uint256).max);
+//         ERC20(usdc).approve(address(PERMIT2), type(uint256).max);
+//         PERMIT2.approve(weth9, address(universalRouter), type(uint160).max, type(uint48).max);
+//         PERMIT2.approve(usdc, address(universalRouter), type(uint160).max, type(uint48).max);
+//     }
+
+//     function test_Deploy() public {
+//         assertEq(address(swapper.universalRouter()), universalRouter);
+//         assertEq(address(swapper.uniswapV3Factory()), address(uniswapV3Factory));
+//     }
+
+//     function test_Swap() public {
+        
+//         assertEq(vm.activeFork(), mainnetFork);
+
+//         console.log(uniswapV2Factory.getPair(usdc, weth9));
+        
+//         // console.log(uniswapV3Factory.getPool(weth9, usdc, 3000));
+//         deal(alice, 10 ether);
+//         deal(weth9, alice, 10 ether);
+//         deal(usdc, alice, 10 ether);
+//         // assertEq(ERC20(weth9).balanceOf(alice), 10 ether);
+//         // assertEq(ERC20(usdc).balanceOf(alice), 0);
+
+//         vm.startPrank(alice);
+//         swapper.swap(weth9, usdc, 1 ether);
+//     }
+// }
 
 
 
