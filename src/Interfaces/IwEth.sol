6599c470dde65c9baa0703e453ccb41af20fb80c
Gareth Larkan <gareth@cis.za.net>
2025-01-13 14:50:14 +0200

    Updated skeleton

--- src/Interfaces/IwEth.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-interface IwEth {
-    function deposit() external payable;
-    function withdraw(uint256 _amount) external;
-}
--- test/TestSetup.sol
+++ test/TestSetup.sol
@@ -2,33 +2,14 @@
 pragma solidity ^0.8.22;
 
 import "forge-std/Test.sol";
-import "../src/PorscheSale.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TestSetup is Test {
 
     address owner = vm.addr(1);
     address alice = vm.addr(2);
     address bob = vm.addr(3);
-    address wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-
-    PorscheSale public porscheSale;
-    PorscheSale.Porsche public porscheCayman;
-
-    event PorscheAdded(uint256 id);
-    event PorschePurchased(uint256 id);
 
     function setUpTests() public {
-        porscheSale = new PorscheSale(wethAddress);
-        porscheCayman = PorscheSale.Porsche({
-            model: "718 Cayman",
-            ipfsHash: "0xmd03ndn03jmf4004f03n40rfbkndnn04ndnlk",
-            price: 30 ether,
-            year: 2006,
-            mileage: 73405,
-            owner: alice,
-            purchaser: address(0),
-            sold: false
-        });
+        
     }
 }
