8d7a22190e0cd612b90d8ec417a951fd12a6ff93
Gareth Larkan <gareth@cis.za.net>
2025-01-13 14:56:44 +0200

    Skeleton start game function

--- src/BlackJack.sol
+++ src/BlackJack.sol
@@ -2,4 +2,14 @@
 pragma solidity 0.8.22;
 
 contract BlackJack {
+
+    uint8 public minimumBet = 0.0001 ether;
+
+    constructor() public {
+
+    }
+
+    function startGame() external payable {
+        require(msg.value >= minimumBet, "Bet too small");
+    }
 }
