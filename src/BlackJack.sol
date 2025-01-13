5959984b5b88262be5dc5f793e964a9cbaa010f3
Gareth Larkan <gareth@cis.za.net>
2025-01-13 15:01:47 +0200

    Create minimum bet functionality and owner functionality to change it

--- src/BlackJack.sol
+++ src/BlackJack.sol
@@ -1,15 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-contract BlackJack {
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    uint8 public minimumBet = 0.0001 ether;
+contract BlackJack is Ownable {
 
-    constructor() public {
+    uint256 public minimumBet = 0.0001 ether;
+
+    event MinimumBetUpdated(uint256 newMinimumBet);
+
+    constructor() Ownable(msg.sender) public {
 
     }
 
     function startGame() external payable {
         require(msg.value >= minimumBet, "Bet too small");
+    }
+
+    function updateMinimumBet(uint256 _newMinimumBet) external onlyOwner {
+        minimumBet = _newMinimumBet;
+
+        emit MinimumBetUpdated(_newMinimumBet);
     }
 }
